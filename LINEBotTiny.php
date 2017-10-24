9c25629433d8a4f09a1745b2e8c4cc8167f4b93b
t12383730 <t12383730@gmail.com>
2017-10-24 23:51:11 +0800

    Update LINEBotTiny.php

--- LINEBotTiny.php
+++ LINEBotTiny.php
@@ -91,8 +91,8 @@ class LINEBotTiny
         ));
         $response = file_get_contents('https://api.line.me/v2/bot/message/reply', false, $context);
         if (strpos($http_response_header[0], '200') === false) {
-            #http_response_code(500);
-            error_log("Request failed: " . $response);
+            http_response_code(500);
+            //error_log("Request failed: " . $response);
         }
     }
     private function sign($body)
@@ -102,3 +102,4 @@ class LINEBotTiny
         return $signature;
     }
 }
+?>
