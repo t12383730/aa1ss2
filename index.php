fe588ecaf0e18e063d280401be8fbaca9102fb9b
t12383730 <t12383730@gmail.com>
2017-10-27 13:09:03 +0800

    Update index.php

--- index.php
+++ index.php
@@ -123,8 +123,8 @@ foreach ($client->parseEvents() as $event) {
                     ) // message
                     )); // $clinet
                     }else{
-                    $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('<channel access token>');
-                    $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '<channel secret>']);
+                    $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('j1p3bvFuEijXmUrDHm3yqZTfi/7ZAhhFwBNL3uKbAMDUE7y2J2pakf9QsChldggWLz4WBlciG266qnemDfM3nYeRGXqaX4aOaHh5hzv7zzAHoD9TB0vVEltC6lgKf9LFFI4CmK3zvpcSpCYDImUiCQdB04t89/1O/w1cDnyilFU=');
+                    $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '00e76b88be7c1520a8616878772ea5d6']);
                     $response = $bot->getProfile('<userId>');
                         if ($response->isSucceeded()) {
                             $profile = $response->getJSONDecodedBody();
