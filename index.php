88b003e3a3f97beb8d720a2903f4ac5b1cf2f549
t12383730 <t12383730@gmail.com>
2017-10-27 09:42:37 +0800

    Update index.php

--- index.php
+++ index.php
@@ -41,34 +41,56 @@ foreach ($client->parseEvents() as $event) {
                     ));*/ /* 回話 */
                     
                     $client->replyMessage(array(
-                    'replyToken' => $event['replyToken'],
-                    'messages' => array(
-                        array(
-                            'type' => 'template', // 訊息類型 (模板)
-                            'altText' => 'Example buttons template', // 替代文字
-                            'template' => array(
-                                'type' => 'buttons', // 類型 (按鈕)
-                                'thumbnailImageUrl' => 'https://api.reh.tw/line/bot/example/assets/images/example.jpg', // 圖片網址 <不一定需要>
-                                'title' => 'Example Menu', // 標題 <不一定需要>
-                                'text' => 'Please select', // 文字
-                                'actions' => array(
-                                    array(
+                        'replyToken' => $event['replyToken'],
+                        'messages' => array(
+                            array(
+                                'type' => 'template', // 訊息類型 (模板)
+                                'altText' => '測試選單', // 替代文字
+                                'template' => array(
+                                    'type' => 'carousel', // 類型 (按鈕)
+                                    'thumbnailImageUrl' => 'https://api.reh.tw/line/bot/example/assets/images/example.jpg', // 圖片網址 <不一定需要>
+                                    'title' => '測試選單', // 標題 <不一定需要>
+                                    'text' => '請選擇下面類別', // 文字
+                                    'actions' => array(
+                                      array(
                                         'type' => 'postback', // 類型 (回傳)
                                         'label' => 'Postback example', // 標籤 1
                                         'data' => 'action=buy&itemid=123' // 資料
-                                    ),
-                                    array(
-                                    'type' => 'message', // 類型 (訊息)
-                                    'label' => 'Message example', // 標籤 2
-                                    'text' => 'Message example' // 用戶發送文字
-                                    ),
-                                    array(
-                                    'type' => 'uri', // 類型 (連結)
-                                    'label' => 'Uri example', // 標籤 3
-                                    'uri' => 'https://github.com/GoneTone/line-example-bot-php' // 連結網址
+                                      ),
+                                      array(
+                                        'type' => 'message', // 類型 (訊息)
+                                        'label' => 'Message example', // 標籤 2
+                                        'text' => 'Message example' // 用戶發送文字
+                                      ),
+                                      array(
+                                        'type' => 'uri', // 類型 (連結)
+                                        'label' => 'Uri example', // 標籤 3
+                                        'uri' => 'https://twdnews.com' // 連結網址
+                                      )
                                     )
+                                ),array(
+                                  'type' => 'carousel', // 類型 (按鈕)
+                                  'thumbnailImageUrl' => 'https://api.reh.tw/line/bot/example/assets/images/example.jpg', // 圖片網址 <不一定需要>
+                                  'title' => '測試選單', // 標題 <不一定需要>
+                                  'text' => '請選擇下面類別', // 文字
+                                  'actions' => array(
+                                    array(
+                                      'type' => 'postback', // 類型 (回傳)
+                                      'label' => 'Postback example', // 標籤 1
+                                      'data' => 'action=buy&itemid=123' // 資料
+                                    ).
+                                    array(
+                                      'type' => 'message', // 類型 (訊息)
+                                      'label' => 'Message example', // 標籤 2
+                                      'text' => 'Message example' // 用戶發送文字
+                                    ),
+                                    array(
+                                      'type' => 'uri', // 類型 (連結)
+                                      'label' => 'Uri example', // 標籤 3
+                                      'uri' => 'https://twdnews.com' // 連結網址
+                                    )  
+                                  )  
                                 )
-                            )
                         )
                     )
                 ));
