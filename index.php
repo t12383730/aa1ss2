df008cd40c7a742784de48deaa9751355d6975d9
t12383730 <t12383730@gmail.com>
2017-10-27 10:17:34 +0800

    Update index.php

--- index.php
+++ index.php
@@ -41,63 +41,63 @@ foreach ($client->parseEvents() as $event) {
                     )); 回話 */
                     
                     $client->replyMessage(array(
-                    'replyToken' => $event['replyToken'],
-                    'messages' => array(
+        'replyToken' => $event['replyToken'],
+        'messages' => array(
+            array(
+                'type' => 'template', // 訊息類型 (模板)
+                'altText' => 'Example buttons template', // 替代文字
+                'template' => array(
+                    'type' => 'carousel', // 類型 (旋轉木馬)
+                    'columns' => array(
                         array(
-                            'type' => 'template',
-                            'altText' => '大家好',
-                            'template' => array(
-                              'type' => 'carousel',
-                              'columns' => array(
+                            'thumbnailImageUrl' => 'https://api.reh.tw/line/bot/example/assets/images/example.jpg', // 圖片網址 <不一定需要>
+                            'title' => 'Example Menu 1', // 標題 1 <不一定需要>
+                            'text' => 'Description 1', // 文字 1
+                            'actions' => array(
                                 array(
-                                    'thumbnailImageUrl' => 'https://example.com/bot/images/item1.jpg',
-                                    'title' => '這是目錄',
-                                    'text' => '選擇內容',
-                                    'actions' => array(
-                                      array(
-                                        'type' => 'postback',
-                                        'label' => 'Buy',
-                                        'data' => 'action=buy&itemid=111'
-                                      ),
-                                      array(
-                                        'type' => 'message',
-                                        'label' => '消息',
-                                        'text' => '發送消息'  
-                                      ),
-                                      array(
-                                        "type": "uri",
-                                        "label": "健康文摘",
-                                        "uri": "http://twdnews.com/"
-                                      )  
-                                    ) // 'actions 括號
-                                ), 
+                                    'type' => 'postback', // 類型 (回傳)
+                                    'label' => 'postback 1', // 標籤 1
+                                    'data' => 'action=buy&itemid=123' // 資料
+                                ),
                                 array(
-                                   'thumbnailImageUrl' => 'https://example.com/bot/images/item1.jpg',
-                                    'title' => '這是目錄',
-                                    'text' => '選擇內容',
-                                    'actions' => array(
-                                      array(
-                                        'type' => 'postback',
-                                        'label' => 'Buy',
-                                        'data' => 'action=buy&itemid=111'
-                                      ),
-                                      array(
-                                        'type' => 'message',
-                                        'label' => '消息',
-                                        'text' => '發送消息'  
-                                      ),
-                                      array(
-                                        "type": "uri",
-                                        "label": "健康文摘",
-                                        "uri": "http://twdnews.com/"
-                                      )  
-                                    ) // 'actions 括號 
-                                )  
-                              )// 'columns' 括號
-                            ) // ''template 括號
-                        ) // 'messages' 下面 array 括號
-                      ) //'messages' array括號
-                    )); //$client雙括號 
+                                    'type' => 'message', // 類型 (訊息)
+                                    'label' => 'Message example 1', // 標籤 2
+                                    'text' => 'Message example 1' // 用戶發送文字
+                                ),
+                                array(
+                                    'type' => 'uri', // 類型 (連結)
+                                    'label' => 'Uri example 1', // 標籤 3
+                                    'uri' => 'https://github.com/GoneTone/line-example-bot-php' // 連結網址
+                                )
+                            )
+                        ),
+                        array(
+                            'thumbnailImageUrl' => 'https://api.reh.tw/line/bot/example/assets/images/example.jpg', // 圖片網址 <不一定需要>
+                            'title' => 'Example Menu 2', // 標題 2 <不一定需要>
+                            'text' => 'Description 2', // 文字 2
+                            'actions' => array(
+                                array(
+                                    'type' => 'postback', // 類型 (回傳)
+                                    'label' => 'postback 2', // 標籤 1
+                                    'data' => 'action=buy&itemid=123' // 資料
+                                ),
+                                array(
+                                    'type' => 'message', // 類型 (訊息)
+                                    'label' => 'Message example 2', // 標籤 2
+                                    'text' => 'Message example 2' // 用戶發送文字
+                                ),
+                                array(
+                                    'type' => 'uri', // 類型 (連結)
+                                    'label' => 'Uri example 2', // 標籤 3
+                                    'uri' => 'https://github.com/GoneTone/line-example-bot-php' // 連結網址
+                                )
+                            )
+                        )
+                    )
+                )
+            )
+        )
+    ));
                     
                     //require_once('includes/text.php'); // Type: Text
                     //require_once('includes/image.php'); // Type: Image
