60ca585eb41e0b5eb57509db69ceb43e4be16388
t12383730 <t12383730@gmail.com>
2017-10-27 13:18:31 +0800

    Update index.php

--- index.php
+++ index.php
@@ -28,6 +28,17 @@ foreach ($client->parseEvents() as $event) {
     switch ($event['type']) {
         case 'message':
             $message = $event['message'];
+            
+            $bot = new \LINE\LINEBot(new CurlHTTPClient('j1p3bvFuEijXmUrDHm3yqZTfi/7ZAhhFwBNL3uKbAMDUE7y2J2pakf9QsChldggWLz4WBlciG266qnemDfM3nYeRGXqaX4aOaHh5hzv7zzAHoD9TB0vVEltC6lgKf9LFFI4CmK3zvpcSpCYDImUiCQdB04t89/1O/w1cDnyilFU='),
+                                     ['channelSecret' => '00e76b88be7c1520a8616878772ea5d6']);
+            $res = $bot->getProfile('user-id');
+            if ($res->isSucceeded()) {
+                $profile = $res->getJSONDecodedBody();
+                $displayName = $profile['displayName'];
+                $statusMessage = $profile['statusMessage'];
+                $pictureUrl = $profile['pictureUrl'];
+            }
+            
             switch ($message['type']) {
                 case 'text':
                     if($message['text'] == '1'){
@@ -123,19 +134,13 @@ foreach ($client->parseEvents() as $event) {
                     ) // message
                     )); // $clinet
                     }else{
-                    $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('j1p3bvFuEijXmUrDHm3yqZTfi/7ZAhhFwBNL3uKbAMDUE7y2J2pakf9QsChldggWLz4WBlciG266qnemDfM3nYeRGXqaX4aOaHh5hzv7zzAHoD9TB0vVEltC6lgKf9LFFI4CmK3zvpcSpCYDImUiCQdB04t89/1O/w1cDnyilFU=');
-                    $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '00e76b88be7c1520a8616878772ea5d6']);
-                    $response = $bot->getProfile('<userId>');
-                        if ($response->isSucceeded()) {
-                            $profile = $response->getJSONDecodedBody();
-                        }                       
                         
                     $client->replyMessage(array(
                     'replyToken' => $event['replyToken'],
                     'messages' => array(
                         array(
                           'type' => 'text',
-                          'text' => $profile['displayName']
+                          'text' => $displayName
                        )
                     )
                     )); 
