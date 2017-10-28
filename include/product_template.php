bbef44c7f9b34d7b7f7997368e4afc386e769d91
t12383730 <t12383730@gmail.com>
2017-10-28 23:19:01 +0800

    Update product_template.php

--- include/product_template.php
+++ include/product_template.php
@@ -4,51 +4,51 @@
         'messages' => array(
             array(
                 'type' => 'template', // 訊息類型 (模板)
-                'altText' => 'Example buttons template', // 替代文字
+                'altText' => '產品介紹', // 替代文字
                 'template' => array(
                     'type' => 'carousel', // 類型 (旋轉木馬)
                     'columns' => array(
                         array(
-                            'thumbnailImageUrl' => 'https://api.reh.tw/line/bot/example/assets/images/example.jpg', // 圖片網址 <不一定需要>
-                            'title' => 'Example Menu 1', // 標題 1 <不一定需要>
-                            'text' => 'Description 1', // 文字 1
+                            'thumbnailImageUrl' => 'http://twdnews.com/image/banner/%E7%9B%8A%E6%AF%8D%E8%8D%89%E7%B4%A0.jpg', // 圖片網址 <不一定需要>
+                            'title' => '益母草素', // 標題 1 <不一定需要>
+                            'text' => '九龍齋益母草素', // 文字 1
                             'actions' => array(
                                 array(
                                     'type' => 'postback', // 類型 (回傳)
-                                    'label' => 'postback 1', // 標籤 1
+                                    'label' => '立即訂購', // 標籤 1
                                     'data' => 'action=buy&itemid=123' // 資料
                                 ),
                                 array(
                                     'type' => 'message', // 類型 (訊息)
-                                    'label' => 'Message example 1', // 標籤 2
-                                    'text' => 'Message example 1' // 用戶發送文字
+                                    'label' => '訊息回傳', // 標籤 2
+                                    'text' => '訊息回傳' // 用戶發送文字
                                 ),
                                 array(
                                     'type' => 'uri', // 類型 (連結)
-                                    'label' => 'Uri example 1', // 標籤 3
-                                    'uri' => 'https://github.com/GoneTone/line-example-bot-php' // 連結網址
+                                    'label' => '查看更多', // 標籤 3
+                                    'uri' => 'http://www.google.com' // 連結網址
                                 )
                             )
                         ),
                         array(
-                            'thumbnailImageUrl' => 'https://api.reh.tw/line/bot/example/assets/images/example.jpg', // 圖片網址 <不一定需要>
-                            'title' => 'Example Menu 2', // 標題 2 <不一定需要>
-                            'text' => 'Description 2', // 文字 2
+                            'thumbnailImageUrl' => 'http://twdnews.com/image/banner/%E9%BB%91%E8%9C%9C%E6%A3%97.jpg', // 圖片網址 <不一定需要>
+                            'title' => '加州黑蜜棗', // 標題 2 <不一定需要>
+                            'text' => '九龍齋加州黑蜜棗', // 文字 2
                             'actions' => array(
                                 array(
                                     'type' => 'postback', // 類型 (回傳)
-                                    'label' => 'postback 2', // 標籤 1
+                                    'label' => '立即訂購', // 標籤 1
                                     'data' => 'action=buy&itemid=123' // 資料
                                 ),
                                 array(
                                     'type' => 'message', // 類型 (訊息)
-                                    'label' => 'Message example 2', // 標籤 2
-                                    'text' => 'Message example 2' // 用戶發送文字
+                                    'label' => '訊息回傳', // 標籤 2
+                                    'text' => '訊息回傳' // 用戶發送文字
                                 ),
                                 array(
                                     'type' => 'uri', // 類型 (連結)
-                                    'label' => 'Uri example 2', // 標籤 3
-                                    'uri' => 'https://github.com/GoneTone/line-example-bot-php' // 連結網址
+                                    'label' => '查看更多', // 標籤 3
+                                    'uri' => 'http://tw.yahoo.com' // 連結網址
                                 )
                             )
                         )
