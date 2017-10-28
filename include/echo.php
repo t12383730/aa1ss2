8d19259711c96717339d06c8f9c64be746400b19
t12383730 <t12383730@gmail.com>
2017-10-28 22:31:51 +0800

    .

--- include/echo.php
+++ include/echo.php
@@ -4,7 +4,7 @@
         'messages' => array(
             array(
                 'type' => 'text', // 訊息類型 (文字)
-                'text' => 'Hello, world!' // 回復訊息
+                'text' => '歡迎來到測試\n輸入1:歡迎訊息\n輸入2:全部內容' // 回復訊息
             )
         )
     ));
