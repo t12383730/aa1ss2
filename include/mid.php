d38b1bf5a679645cb988ab82e5a7dc91e645d06e
t12383730 <t12383730@gmail.com>
2017-11-13 00:10:33 +0800

    Update mid.php

--- include/mid.php
+++ include/mid.php
@@ -29,11 +29,4 @@ $client->replyMessage(array(
      )
    )
 ));
-
-$myfile = fopen("try.txt", "w") or die("Unable to open file!");
-$txt = "Mickey Mouse\n";
-fwrite($myfile, $txt);
-$txt = "Minnie Mouse\n";
-fwrite($myfile, $txt);
-fclose($myfile);
 ?>
