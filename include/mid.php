f7a0bee92657729c7314baaa0e8b395e5dae658b
t12383730 <t12383730@gmail.com>
2017-11-12 23:46:57 +0800

    Update mid.php

--- include/mid.php
+++ include/mid.php
@@ -1,6 +1,6 @@
 <?php
 
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('j1p3bvFuEijXmUrDHm3yqZTfi/7ZAhhFwBNL3uKbAMDUE7y2J2pakf9QsChldggWLz4WBlciG266qnemDfM3nYeRGXqaX4aOaHh5hzv7zzAHoD9TB0vVEltC6lgKf9LFFI4CmK3zvpcSpCYDImUiCQdB04t89/1O/w1cDnyilFU=');
+/*$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('j1p3bvFuEijXmUrDHm3yqZTfi/7ZAhhFwBNL3uKbAMDUE7y2J2pakf9QsChldggWLz4WBlciG266qnemDfM3nYeRGXqaX4aOaHh5hzv7zzAHoD9TB0vVEltC6lgKf9LFFI4CmK3zvpcSpCYDImUiCQdB04t89/1O/w1cDnyilFU=');
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '00e76b88be7c1520a8616878772ea5d6']);
 $response = $bot->getProfile('U206d25c2ea6bd87c17655609a1c37cb8');
 if ($response->isSucceeded()) {
@@ -8,7 +8,9 @@ if ($response->isSucceeded()) {
     $displayName = $profile['displayName'];
     $pictureUrl = $profile['pictureUrl'];
     $statusMessage $profile['statusMessage'];
-}
+}*/
+
+$$displayName='123';
 
 $client->replyMessage(array(
   'replyToken' => $event['replyToken'],
