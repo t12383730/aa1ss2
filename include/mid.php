28b2f0d1670472b3f8e59c8b2d9940adac468e48
t12383730 <t12383730@gmail.com>
2017-11-12 23:42:09 +0800

    Update mid.php

--- include/mid.php
+++ include/mid.php
@@ -2,7 +2,7 @@
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('j1p3bvFuEijXmUrDHm3yqZTfi/7ZAhhFwBNL3uKbAMDUE7y2J2pakf9QsChldggWLz4WBlciG266qnemDfM3nYeRGXqaX4aOaHh5hzv7zzAHoD9TB0vVEltC6lgKf9LFFI4CmK3zvpcSpCYDImUiCQdB04t89/1O/w1cDnyilFU=');
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '00e76b88be7c1520a8616878772ea5d6']);
-$response = $bot->getProfile('<userId>');
+$response = $bot->getProfile('U206d25c2ea6bd87c17655609a1c37cb8');
 if ($response->isSucceeded()) {
     $profile = $response->getJSONDecodedBody();
     $displayName = $profile['displayName'];
