c719589166d171e306e80e822d4be51f72a6dac8
t12383730 <t12383730@gmail.com>
2017-11-12 22:31:46 +0800

    Update mid.php

--- include/mid.php
+++ include/mid.php
@@ -1,5 +1,5 @@
 <?php
-$bot = new \LINE\LINEBot(new CurlHTTPClient('j1p3bvFuEijXmUrDHm3yqZTfi/7ZAhhFwBNL3uKbAMDUE7y2J2pakf9QsChldggWLz4WBlciG266qnemDfM3nYeRGXqaX4aOaHh5hzv7zzAHoD9TB0vVEltC6lgKf9LFFI4CmK3zvpcSpCYDImUiCQdB04t89/1O/w1cDnyilFU='), [
+/*$bot = new \LINE\LINEBot(new CurlHTTPClient('j1p3bvFuEijXmUrDHm3yqZTfi/7ZAhhFwBNL3uKbAMDUE7y2J2pakf9QsChldggWLz4WBlciG266qnemDfM3nYeRGXqaX4aOaHh5hzv7zzAHoD9TB0vVEltC6lgKf9LFFI4CmK3zvpcSpCYDImUiCQdB04t89/1O/w1cDnyilFU='), [
     'channelSecret' => '00e76b88be7c1520a8616878772ea5d6'
 ]);
 
@@ -9,22 +9,22 @@ if ($res->isSucceeded()) {
     $displayName = $profile['displayName'];
     $statusMessage = $profile['statusMessage'];
     $pictureUrl = $profile['pictureUrl'];
-}
+}*/
 
 $client->replyMessage(array(
   'replyToken' => $event['replyToken'],
   'messages' => array(
-     array(
+     /*array(
        'type' => 'text',
        'text' => $displayName
      ),
      array(
        'type' => 'text',
        'text' => $statusMessage
-     ),
+     ),*/
      array(
        'type' => 'text',
-       'text' => $pictureUrl
+       'text' => 'HELLO'
      )
    )
 ));
