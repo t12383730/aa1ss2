b3f1aeacca6dfa608a2b2fffb585a8f37f4cf534
t12383730 <t12383730@gmail.com>
2017-12-03 21:58:21 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,7 @@
 <?php
 
 return [
-    'channelAccessToken' => 'j1p3bvFuEijXmUrDHm3yqZTfi/7ZAhhFwBNL3uKbAMDUE7y2J2pakf9QsChldggWLz4WBlciG266qnemDfM3nYeRGXqaX4aOaHh5hzv7zzAHoD9TB0vVEltC6lg
-
-Kf9LFFI4CmK3zvpcSpCYDImUiCQdB04t89/1O/w1cDnyilFU=', // Channel Access Token
+    'channelAccessToken' => 'j1p3bvFuEijXmUrDHm3yqZTfi/7ZAhhFwBNL3uKbAMDUE7y2J2pakf9QsChldggWLz4WBlciG266qnemDfM3nYeRGXqaX4aOaHh5hzv7zzAHoD9TB0vVEltC6lgKf9LFFI4CmK3zvpcSpCYDImUiCQdB04t89/1O/w1cDnyilFU=', // Channel Access Token
     'channelSecret' => '00e76b88be7c1520a8616878772ea5d6' // Channel Secret
 ];
 ?>
