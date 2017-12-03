2407d21c086caaba22c67c3855a304f160b0c901
t12383730 <t12383730@gmail.com>
2017-12-03 21:42:00 +0800

    Update index.php

--- index.php
+++ index.php
@@ -29,15 +29,68 @@ foreach ($client->parseEvents() as $event) {
         case 'message':
             $message = $event['message'];
             
+            $json = file_get_contents('https://spreadsheets.google.com/feeds/list/1tQCaj3LUVwH0tBuPrfBY2dOJuF-qzpYEdOqGdNvJRLc/od6/public/values?alt=json');
+            $data = json_decode($json, true);
+            $result = array();
+ 
+            foreach ($data['feed']['entry'] as $item) {
+              $keywords = explode(',', $item['gsx$keyword']['$t']);
+              foreach ($keywords as $keyword) {
+                if (mb_strpos($message['text'], $keyword) !== false) {
+                  $candidate = array(
+                    'thumbnailImageUrl' => $item['gsx$photourl']['$t'],
+                    'title' => $item['gsx$title']['$t'],
+                    'text' => $item['gsx$title']['$t'],
+                    'actions' => array(
+                      array(
+                      'type' => 'uri',
+                      'label' => '查看詳情',
+                      'uri' => $item['gsx$url']['$t'],
+                      ),
+                    ),
+                  );
+                  array_push($result, $candidate);
+                }
+              }
+            }
+            
             switch ($message['type']) {
                 case 'text':
-                    if($message['text'] == '2'){
+                    /*if($message['text'] == '2'){
                         require_once('include/product_template.php');
                     }else if($message['text'] == '3'){
                         require_once('include/mid.php');
                     }else{
                         require_once('include/help.php');
-                    }      
+                    }*/
+                    
+                    $client->replyMessage(array(
+                        'replyToken' => $event['replyToken'],
+                        'messages' => array(
+                            array(
+                                'type' => 'text',
+                                'text' => $message['text'].'讓我想想喔…',
+                            ),
+                            array(
+                                'type' => 'template',
+                                'altText' => '為您推薦下列美食：',
+                                'template' => array(
+                                    'type' => 'carousel',
+                                    'columns' => $result,
+                                ),
+                            ),
+                            array(
+                                'type' => 'text',
+                                'text' => '這些都超好吃，真心不騙！',
+                            ),
+                            array(
+                                'type' => 'sticker',
+                                'packageId' => '1',
+                                'stickerId' => '2',
+                            ),
+                        ),
+                    ));
+                    
                     break;
                 default:
                     require_once('include/welcome.php'); 
