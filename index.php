596baabc7c484ba19f5bed0dc25e57bdcd00a971
t12383730 <t12383730@gmail.com>
2017-12-03 21:05:58 +0800

    Update index.php

--- index.php
+++ index.php
@@ -41,7 +41,7 @@ foreach ($client->parseEvents() as $event) {
                           'messages' => array(
                             array(
                             'type' => 'text',
-                            'text' => '$message'
+                            'text' => $message['text']
                             )
                           )
                         )); 
