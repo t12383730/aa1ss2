23664f9fecda7eaffc80aa53b915fdc956b358bb
t12383730 <t12383730@gmail.com>
2017-12-03 22:01:04 +0800

    Update index.php

--- index.php
+++ index.php
@@ -4,27 +4,6 @@ date_default_timezone_set("Asia/Taipei"); // 設定時區為台北時區
 require_once('LINEBotTiny.php');
 $channelAccessToken='j1p3bvFuEijXmUrDHm3yqZTfi/7ZAhhFwBNL3uKbAMDUE7y2J2pakf9QsChldggWLz4WBlciG266qnemDfM3nYeRGXqaX4aOaHh5hzv7zzAHoD9TB0vVEltC6lgKf9LFFI4CmK3zvpcSpCYDImUiCQdB04t89/1O/w1cDnyilFU=';
 $channelSecret='00e76b88be7c1520a8616878772ea5d6';
-/*if (file_exists(__DIR__ . '/config.php')) {
-    $config = include __DIR__ . '/config.php'; // 引入設定檔
-    if ($config['channelAccessToken'] == Null || $config['channelSecret'] == Null) {
-        error_log("config.php 設定檔內的驗證權杖和粉絲專頁存取權杖尚未設定完全！", 0); // 輸出錯誤
-    } else {
-        $channelAccessToken = $config['channelAccessToken'];
-        $channelSecret = $config['channelSecret'];
-    }
-} else {
-    $configFile = fopen("config.php", "w") or die("Unable to open file!");
-    $configFileContent = "<?php
-
-return [
-    'channelAccessToken' => '',
-    'channelSecret' => ''
-];
-?>";
-    fwrite($configFile, $configFileContent); // 建立文件並寫入
-    fclose($configFile); // 關閉文件
-    error_log("config.php 設定檔建立成功，請編輯檔案輸入驗證權杖和粉絲專頁存取權杖！", 0); // 輸出錯誤
-}*/
 
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
 foreach ($client->parseEvents() as $event) {
