4a70f8a267aaa4495e0609db8c0ee180e9bdb948
t12383730 <t12383730@gmail.com>
2017-12-03 21:23:54 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-/*date_default_timezone_set("Asia/Taipei"); // 設定時區為台北時區
+date_default_timezone_set("Asia/Taipei"); // 設定時區為台北時區
 require_once('LINEBotTiny.php');
 if (file_exists(__DIR__ . '/config.php')) {
     $config = include __DIR__ . '/config.php'; // 引入設定檔
@@ -51,71 +51,6 @@ foreach ($client->parseEvents() as $event) {
             require_once('include/join.php'); 
             break;
     }
-};*/
-require_once('LINEBotTiny.php');
-// 重要資訊1
-$channelSecret = "00e76b88be7c1520a8616878772ea5d6";
-// 重要資訊2
-$channelAccessToken = "j1p3bvFuEijXmUrDHm3yqZTfi/7ZAhhFwBNL3uKbAMDUE7y2J2pakf9QsChldggWLz4WBlciG266qnemDfM3nYeRGXqaX4aOaHh5hzv7zzAHoD9TB0vVEltC6lg
-Kf9LFFI4CmK3zvpcSpCYDImUiCQdB04t89/1O/w1cDnyilFU=";
-// Google表單資料
-$googledataspi = "https://spreadsheets.google.com/feeds/list/1acQdkyKOS-L_Jsp__flWZ69tzUh6aKDBYKc50pq16No/od6/public/values?alt=json";
-
-// 建立Client from LINEBotTiny
-$client = new LINEBotTiny($channelAccessToken, $channelSecret);
-
-// 取得事件(只接受文字訊息)
-foreach ($client->parseEvents() as $event) {
-switch ($event['type']) {       
-    case 'message':
-        // 讀入訊息
-        $message = $event['message'];
-
-        // 將Google表單轉成JSON資料
-        /*$json = file_get_contents($googledataspi);
-        $data = json_decode($json, true);           
-        $store_text=''; 
-        // 資料起始從feed.entry          
-        foreach ($data['feed']['entry'] as $item) {
-            // 將keywords欄位依,切成陣列
-            $keywords = explode(',', $item['gsx$keywords']['$t']);
-
-            // 以關鍵字比對文字內容，符合的話將店名/地址寫入
-            foreach ($keywords as $keyword) {
-                if (mb_strpos($message['text'], $keyword) !== false) {                      
-                    $store_text = $item['gsx$storename']['$t']." 地址是:".$item['gsx$storeaddress']['$t'];                 
-              }
-            }
-        }*/       
-        
-        switch ($message['type']) {
-            case 'text':
-                // 回覆訊息
-                // 第一段 你要想找_(原字串)_ 讓我想想喔…
-                // 第二段 介紹你_______不錯喔
-                $client->replyMessage(array(
-                    'replyToken' => $event['replyToken'],
-                    'messages' => array(
-                        array(
-                            'type' => 'text',
-                            'text' => '你想要找'.$message['text'].' 讓我想想喔…',
-                        ),
-                        array(
-                            'type' => 'text',
-                            'text' => '介紹你 '.$store_text.' 不錯喔',
-                        )
-
-                    ),
-                ));               
-                break;
-            default:
-                error_log("Unsupporeted message type: " . $message['type']);
-                break;
-        }
-        break;
-    default:
-        error_log("Unsupporeted event type: " . $event['type']);
-        break;
-}
+};
 
 ?>
