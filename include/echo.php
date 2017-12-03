0aa9f592a1330bb283a52f7e0059d013fba49238
t12383730 <t12383730@gmail.com>
2017-12-03 21:07:52 +0800

    Create echo.php

---
+++ include/echo.php
@@ -0,0 +1,13 @@
+<?php
+
+$client->replyMessage(array(
+  'replyToken' => $event['replyToken'],
+  'messages' => array(
+    array(
+      'type' => 'text',
+      'text' => $message['text']
+    )
+  )
+)); 
+
+?>
